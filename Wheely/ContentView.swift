d0fabdaba0f1fac7a283e73d29fd2992b44f9f60
lokingdie111 <lokingdie111@naver.com>
2025-07-11 15:56:19 +0900

    Initial Commit

---
+++ Wheely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wheely
+//
+//  Created by 민현규 on 7/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wheely/WheelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelyApp.swift
+//  Wheely
+//
+//  Created by 민현규 on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WheelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
